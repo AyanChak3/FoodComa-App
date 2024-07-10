e3a81e9bc673b411b916a4cbdabff9ca4e0a3a8a
AyanChak3 <>
2024-07-10 17:12:23 +0530

    Added auth token

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const ServerConfig = require('./config/serverConfig')
 const connectDB = require('./config/dbConfig');
 const userRouter = require('./routes/userRoute');
 const cartRouter = require('./routes/cartRoute');
 const authRouter = require('./routes/authRoute');
 const app = express();
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({extended : true}))
@@ -15,6 +17,7 @@ app.use('/carts',cartRouter)
 app.use('/auth',authRouter)
 app.post('/ping',(req,res)=>{
     console.log(req.body)
+    console.log(req.cookies)
     return res.json({message : "pong"})
 })
 app.listen(ServerConfig.PORT,async()=>{
